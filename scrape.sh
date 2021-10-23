e18ab03f11ccac84bea5419e47cbbe2c22cef7cd
Matt Schlosser <hello@mattschlosser.me>
2021-10-23 15:24:44 -0600

    add percent stat

--- scrape.sh
+++ scrape.sh
@@ -18,7 +18,15 @@ sqlite3 data.db >> new-out.csv <<EOF
 .mode csv
 select k.\`alternate name\` from missing m left join (select m.id, a.\`alternate name\` as \`alternate name\`  from missing m left join alternatePlaces a on a.geonameid = m.id and a.isoLanguage = '$COTW_LANG' group by m.id) k on k.id = m.id;
 EOF
+EMPTY=$(cat new-out.csv | sed '/^\s*$/d' | wc -l)
+TOTAL=$(cat new-out.csv | wc -l)
+echo "Translations found for $EMPTY of $TOTAL places"
+PERCENT=$(echo "scale=2; $EMPTY/$TOTAL*100" | bc)
+echo "That's $PERCENT%!"
+echo ""
 # output format into standard csv
 cat "$1" | awk -F, '{print $1 FS $2}' > data-key.csv
 paste -d, data-key.csv new-out.csv > "$1-final.csv";
+# displays a preview of the final file
+echo "Here's a quick preview..."
 head "$1-final.csv"
