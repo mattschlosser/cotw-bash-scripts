a71f8784694f76b49c6db5725f722dd942627aa2
Matt Schlosser <hello@mattschlosser.me>
2021-10-23 20:16:15 -0600

    add 23 countries to get-all script

--- get-all.sh
+++ get-all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-COUNTRIES=("AZ" "BD" "BT" "KH" "EC" "EG" "ET" "IN" "LA" "ID" "JO" "KZ" "NP" "TH")
+# downloads all 23 countries geonames data into a local sqlite3 db
+
+COUNTRIES=("AZ" "BD" "BT" "KH" "EC" "EG" "ET" "IN" "LA" "ID" "JO" "KZ" "KG" "NP" "PK" "ES" "LK" "SD" "TZ" "TH" "TG" "UG" "VN")
 
 for i in "${COUNTRIES[@]}"
 do  ./get.sh "$i"
-done
+done    
+
+# THe resulting DB is ~628 MB
--- scrape.sh
+++ scrape.sh
@@ -2,7 +2,7 @@
 INPUT_FILE=$1
 COTW_LANG=$(echo "$1" | awk -F- '{print $2}')
 # echo "Detected lang code: $COTW_LANG";
-
+START=$(date +%s.%N)
 # COTW_LANG=th
 # create a file with just geonames id
 tail -n +2 "$1" |  awk -F, '{print $1}' | awk -F- '{print $NF}' | tr -d \" > missing.csv
@@ -18,16 +18,18 @@ sqlite3 data.db >> new-out.csv <<EOF
 .mode csv
 select k.\`alternate name\` from missing m left join (select m.id, a.\`alternate name\` as \`alternate name\`  from missing m left join alternatePlaces a on a.geonameid = m.id and a.isoLanguage = '$COTW_LANG' group by m.id) k on k.id = m.id;
 EOF
-EMPTY=$(cat new-out.csv | sed '/^\s*$/d' | wc -l)
-TOTAL=$(cat new-out.csv | wc -l)
+EMPTY=$(tail -n +2 new-out.csv | sed '/^\s*$/d' | wc -l)
+TOTAL=$(tail -n +2 new-out.csv | wc -l)
 #echo "Translations found for $EMPTY of $TOTAL places"
 PERCENT=$(echo "scale=2; $EMPTY/$TOTAL*100" | bc)
 #echo "That's $PERCENT%!"
 #echo ""
-echo "$1,$EMPTY,$TOTAL,$PERCENT"
 # output format into standard csv
 cat "$1" | awk -F, '{print $1 FS $2}' > data-key.csv
 paste -d, data-key.csv new-out.csv > "$1-final.csv";
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "$1,$EMPTY,$TOTAL,$PERCENT,$DIFF"
 # displays a preview of the final file
 #echo "Here's a quick preview..."
 # head "$1-final.csv"
